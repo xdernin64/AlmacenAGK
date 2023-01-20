2598237508d9fb2aeeb5248d203a9c599cf10784
xdernin <xdernin@gmail.com>
2023-01-20 16:11:07 -0500

    Detalles error 404

--- src/components/pages/error404.jsx
+++ src/components/pages/error404.jsx
@@ -1,8 +1,17 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Error404 = () => {
     return (
-        <div className="h-screen w-full bg-stone-800 text-white fixed m-auto">
-        <h1>ERROR 404</h1>
-        <p>Página no encontrada</p>
+        <div className="flex h-screen background404 bg-slate-800 ">
+            <div className="m-auto text-center">
+                <h1 className="text-6xl font-mono text-zinc-100">ERROR 404</h1>
+                <img src="https://cdn1.iconfinder.com/data/icons/robotics-astute-vol-1/512/Broken_Robot-512.png" alt="Error404picture" />
+                <h2 className="text-4xl font-mono text-zinc-300">Página no encontrada</h2>
+                <div className="m-10 boton-regresar">   
+                    <Link className="bg-slate-200 text-bold text-xl rounded-xl p-5" to={"/"}>Ir a la página principal</Link>
+                </div>
+                
+            </div>
         </div>
     );
 }
